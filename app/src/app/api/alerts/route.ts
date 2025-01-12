b6513866ff19f09985ed720a300b71e58c2e5742
Pranshu Gupta <pranshu11111@gmail.com>
2025-01-12 22:46:04 +0530

    Update route.ts

--- app/src/app/api/alerts/route.ts
+++ app/src/app/api/alerts/route.ts
@@ -2,5 +2,5 @@ import { alerts } from '@/data/alerts';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  return NextResponse.json(alerts);
+  return NextResponse.json(alerts); //
 }
